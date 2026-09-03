da14ee3a8469befb33a64b6dd902263197eb6400
lawalolamide48-cell <lawalolamide48@gmail.com>
2026-09-03 23:33:37 +0100

    Deduplicate catalogue titles

--- catalogue-core-final.js
+++ catalogue-core-final.js
@@ -1,1 +1,1 @@
-window.CINEVAULT_CATALOG=R.map(([t,y,k,g,pop])=>({t,y,type:k==='s'?'series':'movie',g:g.split('.').map(x=>G[x]||x),pop}));
+window.CINEVAULT_CATALOG=R.map(([t,y,k,g,pop])=>({t,y,type:k==='s'?'series':'movie',g:g.split('.').map(x=>G[x]||x),pop})).filter((m,i,a)=>i===a.findIndex(x=>x.t.toLowerCase()===m.t.toLowerCase()));
